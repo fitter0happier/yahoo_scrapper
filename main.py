113321840a50a47bf490885c035a1a727298b034
Tomchik, Roman <fitter0happier@protonmail.com>
2025-03-12 15:09:13 +0100

    Define structure, add tickers for test

---
+++ main.py
@@ -0,0 +1,43 @@
+import argparse
+import json
+
+from datetime import datetime, timedelta, timezone
+from yahoo_scraper import YahooScraper
+
+def load_tickers(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data['tickers']
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Yahoo Finance Scraper")
+    parser.add_argument('--tickers_file', type=str, default='tickers.json', help='Path to the JSON file containing the list of tickers')
+    parser.add_argument('--start_date', type=str, help='Start date for scraping (YYYY-MM-DD)')
+    parser.add_argument('--end_date', type=str, help='End date for scraping (YYYY-MM-DD)')
+    parser.add_argument('--timedelta', type=int, default=1, help='Time delta for grouping messages in days')
+    parser.add_argument('--bucket_name', type=str, default="training-data-automated", help='S3 bucket name')
+    parser.add_argument('--lambda_name', type=str, default="reddit_scraper_lambda", help='Name of the Lambda function')
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    tickers = load_tickers(args.tickers_file)
+    timedif = timedelta(days=args.timedelta)
+    bucket_name = args.bucket_name
+    lambda_name = args.lambda_name
+
+    if args.start_date:
+        start_date = datetime.strptime(args.start_date, '%Y-%m-%d').replace(tzinfo=timezone.utc)
+    else:
+        start_date = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
+
+    if args.end_date:
+        end_date = datetime.strptime(args.end_date, '%Y-%m-%d').replace(tzinfo=timezone.utc)
+    else:
+        end_date = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
+
+    yahoo_scraper = YahooScraper(tickers, start_date, end_date, timedif, bucket_name=bucket_name, lambda_name=lambda_name)
+
+if __name__ == '__main__':
+    main()
---
+++ yahoo_scraper.py
@@ -0,0 +1,27 @@
+import boto3
+import yfinance as yf
+
+from datetime import datetime, timedelta
+from typing import List
+from queue import Queue
+
+class YahooScraper:
+    def __init__(
+        self, tickers: List[str],
+        start_date: datetime,
+        end_date: datetime, 
+        timedelta: timedelta,
+        bucket_name: str = None,
+        lambda_name: str = "yahoo_scraper_lambda"
+    ):
+        self.tickers = tickers
+        self.start_date = start_date
+        self.end_date = end_date
+        self.timedelta = timedelta
+        self.bucket_name = bucket_name
+        self.lambda_name = lambda_name
+        self.bucket_tmp_dir = "yahoo_tmp"
+        self.s3_client = boto3.client('s3')
+        self.lambda_client = boto3.client('lambda', region_name='us-east-2')
+        self.batch_size = 500
+        self.files_contents_queue = Queue()
